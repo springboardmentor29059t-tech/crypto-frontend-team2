39f688af3d899c0147da51a8c50d4af91ca75bf5
ATTADA AKSHAYA <akshayaattada21@gmail.com>
2026-01-19 14:49:33 +0530

    Remove root-level frontend files; keep only Crypto_Frontend and Crypto_Backend

--- src/components/UI/Icon.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Icon = ({ name, size = 24, className = "" }) => {
-  return <i className={`ph ph-${name} ${className}`} style={{ fontSize: size }}></i>;
-};
-
-export default Icon;
